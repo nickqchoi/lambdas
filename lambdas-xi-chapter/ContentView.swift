ebe5f3c981b01d4061ad51ca956e1d10700bc1c0
Nicholas Choi <nickchoi@umich.edu>
2026-01-25 15:21:07 -0500

    Initial Commit

---
+++ lambdas-xi-chapter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lambdas-xi-chapter
+//
+//  Created by Nicholas Choi on 25/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lambdas-xi-chapter/lambdas_xi_chapterApp.swift
@@ -0,0 +1,17 @@
+//
+//  lambdas_xi_chapterApp.swift
+//  lambdas-xi-chapter
+//
+//  Created by Nicholas Choi on 25/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct lambdas_xi_chapterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
